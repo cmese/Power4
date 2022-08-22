9e751704d30387da0c1cd290a36fd298ca810ab2
chris meseha <cmeseha94@gmail.com>
2022-08-22 10:55:26 -0400

    finishes AI evaluation function but scores are still arbitrary numbers - untested

--- Assets/Scripts/AIManager.cs
+++ Assets/Scripts/AIManager.cs
@@ -14,16 +14,161 @@ public class AIManager {
     //TODO: EvaluateBoardPoints(board) - prioritizes maxiumum points (Blitz game mode) instead of connect 4's
     private float evaluateBoard(int[,] board) {
         float score = 0;
+
+        //Score center column
+        score += getCenterScore(board);
+
         //Horizontal - score potential of horizontal connect 4 in each row
         for (int row = 0; row < rows; row++) {
-            var rowArray = GetRow(board, row);
+            //var rowArray = GetRow(board, row);
             for (int col = 0; col < cols-3; col++) {
-                score += evaluateWindow(rowArray[col..(col+windowLength)]);
+                //score += evaluateWindow(board, rowArray[col..(col+windowLength)]);
+
+                //Vector2Int[] window = getHorizontalWindow(board, row, col);
+                //score += evaluateWindow(board, window);
+                score += getHorizontalScore(board, row, col);
             }
         }
+
         //Vertical Score
-        //Diag Score
+        for (int col = 0; col < cols; col++) {
+            for (int row = 0; row < rows/2; row++) {
+                score += getVerticalScore(board, row, col);
+            }
+        }
+
+        // Diag Scores - checks bottom diags before top ones
+        //Diag Positive Slope. checked right to left. row >=3, col <= 3
+        for (int row = rows/2-1; row >= 0; row--) {
+            for (int col = cols-1; col >= cols/2; col--) {
+                score += getDiagPosScore(board, row, col);
+            }
+        }
+
+        //Diag Negative Slope. checked left to right. row <= 2, col <= 3
+        for (int row = rows/2-1; row >= 0; row--) {
+            for (int col = 0; col <= cols/2; col++) {
+                score += getDiagPosScore(board, row, col);
+            }
+        }
         return score;
+    }
+
+    private float getCountScore(int emptySpace, int aiCount, int oppCount) {
+        float score = 0;
+        // 4/4 in window
+        if (aiCount == 4) score = Mathf.Infinity; //AI win
+        else if (oppCount == 4) score = Mathf.NegativeInfinity; //oppponent win
+
+        // 3/4 in window.
+        else if (aiCount == 3 && emptySpace == 1) score = 1000;
+        else if (oppCount == 3 && emptySpace == 1) score = -1000;
+
+        // 2/4
+        else if (aiCount == 2 && emptySpace == 2) score = 1000;
+        else if (oppCount == 2 && emptySpace == 2) score = 1000;
+
+        return score;
+    }
+
+    private float getCenterScore(int[,] board) {
+        float score = 0;
+        int aiCount = 0, oppCount = 0;
+        for (int row = 0; row < rows; row++) {
+            switch (board[row, cols/2]) {
+                case 0:
+                    break;
+                case 1:
+                    aiCount++; break;
+                case 2:
+                    oppCount++; break;
+            }
+        }
+        score += aiCount * 6;
+        score -= oppCount * 6;
+        return score;
+    }
+
+    private float getHorizontalScore(int[,] board, int row, int col) {
+        int emptySpace = 0, aiCount = 0, oppCount = 0;
+        for (int i = 0; i < windowLength; i++) {
+            switch (board[row, col + i]) {
+                case 0:
+                    emptySpace++;
+                    if (!isReachable(board, row, col + i)) return 0;
+                    break;
+                case 1:
+                    aiCount++;
+                    break;
+                case 2:
+                    oppCount++;
+                    break;
+            }
+        }
+        return getCountScore(emptySpace, aiCount, oppCount);
+    }
+
+    private float getVerticalScore(int[,] board, int row, int col) {
+        int emptySpace = 0, aiCount = 0, oppCount = 0;
+        for (int i = 0; i < windowLength; i++) {
+            switch (board[row + i, col]) {
+                case 0:
+                    emptySpace++;
+                    break;
+                case 1:
+                    aiCount++;
+                    break;
+                case 2:
+                    oppCount++;
+                    break;
+            }
+        }
+        return getCountScore(emptySpace, aiCount, oppCount);
+    }
+
+    //positive slope left to right. checked from top to bottom
+    private float getDiagPosScore(int[,] board, int row, int col) {
+        int emptySpace = 0, aiCount = 0, oppCount = 0;
+        for (int i = 0; i < windowLength; i++) {
+            switch (board[row + i, col - i]) {
+                case 0:
+                    emptySpace++;
+                    break;
+                case 1:
+                    aiCount++;
+                    break;
+                case 2:
+                    oppCount++;
+                    break;
+            }
+        }
+        return getCountScore(emptySpace, aiCount, oppCount);
+    }
+
+    //negative slope left to right. checked from top to bottom
+    private float getDiagNegScore(int[,] board, int row, int col) {
+        int emptySpace = 0, aiCount = 0, oppCount = 0;
+        for (int i = 0; i < windowLength; i++) {
+            switch (board[row + i, col + i]) {
+                case 0:
+                    emptySpace++;
+                    break;
+                case 1:
+                    aiCount++;
+                    break;
+                case 2:
+                    oppCount++;
+                    break;
+            }
+        }
+        return getCountScore(emptySpace, aiCount, oppCount);
+    }
+
+    private Vector2Int[] getHorizontalWindow(int[,] board, int row, int col) {
+        Vector2Int[] window = new Vector2Int[4];
+        for (int i = 0; i < windowLength; i++)
+            window[i] = new Vector2Int(row, col + i);
+        return window;
     }
 
     private int[] GetRow(int[,] board, int row) {
@@ -32,18 +177,52 @@ public class AIManager {
                 .ToArray();
     }
 
-    private float evaluateWindow(int[] window) {
+    // returns true if empty cells in window are reachable
+    private bool isReachable(int[,] board, int row, int col) {
+        if (row + 1 == rows || board[row + 1, col] != 0)
+            return true;
+        else
+            return false;
+    }
+
+    private Tuple<int, int, int> getCounts(int[,] board, Vector2Int[] window) {
+        int emptySpace = 0, aiCount = 0, oppCount = 0;
+        for (int i = 0; i < windowLength; i++) {
+            switch (board[window[i].x, window[i].y]) {
+                case 0:
+                    emptySpace++;
+                    break;
+                case 1:
+                    aiCount++;
+                    break;
+                case 2:
+                    oppCount++;
+                    break;
+            }
+        }
+        return new Tuple<int, int, int>(emptySpace, aiCount, oppCount);
+    }
+
+    private float evaluateWindow(int[,] board, Vector2Int[] window) {
         float score = 0;
+        int aiCount = window.Count(space => board[space.x, space.y] == 1);
+        int oppCount = window.Count(space => board[space.x, space.y] == 2);
+        int emptySpace = window.Count(space => board[space.x, space.y] == 0);
+
         // 4/4 in window
-        if (window.Count(x => x == 1) == 4) score = Mathf.Infinity; //AI win
-        else if (window.Count(x => x == 2) == 4) score = Mathf.NegativeInfinity; //oppponent win
+        if (aiCount == 4) score = Mathf.Infinity; //AI win
+        else if (oppCount == 4) score = Mathf.NegativeInfinity; //oppponent win
+
         // 3/4 in window. TODO: AND is reachable (Figure 7)
-        else if (window.Count(x => x == 1) == 3 && window.Count(x => x == 0) == 1) score = 1000;
-        else if (window.Count(x => x == 2) == 3 && window.Count(x => x == 0) == 1) score = -1000;
-        //TODO: 2/4 AND is reachable
-        //TODO: 1/4 AND is reachable
+        else if (aiCount == 3 && emptySpace == 1) score = 1000;
+        else if (oppCount == 3 && emptySpace == 1) score = -1000;
+
+        // 2/4 TODO: AND is reachable
+        //else if (window.Count(x => x == 1) == 2 && window.Count(x => x == 0) == 2) score = 1000;
+        //else if (window.Count(x => x == 2) == 2 && window.Count(x => x == 0) == 2) score = -1000;
         return score;
     }
+
 
     private float miniMax(int[,] board, int depth, bool isMax) {
         float score = evaluateBoard(board);
